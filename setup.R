2f8de5cb3dd797c876d77350b25bde62f8741dd3
klausfrieler <kf@omniversum.de>
2025-03-14 17:56:41 +0100

    Added more data sets

--- setup.R
+++ setup.R
@@ -1,6 +1,11 @@
 library(tidyverse)
 library(sjPlot)
 library(lmerTest)
+library(performance)
+library(effectsize)
+library(rstatix)
+library(marginaleffects)
+
 source("utils.R")
 source("plot_utils.R")
 source("model_comp.R")
@@ -16,9 +21,18 @@ setup_workspace <- function(){
   mer <- mer %>% mutate(across(all_of(mer_mir_features), function(x) scale(x) %>% as.numeric()))
   top_emo <- mer %>% select(s_id, all_of(mer_emo_vars)) %>% pivot_longer(-s_id) %>% group_by(s_id, name) %>% summarise(m = mean(value)) %>% summarise(emo = name[which.max(m)], .groups = "drop") %>% ungroup()
   mer <- mer %>% left_join(top_emo, by = "s_id")
+  kid_beats <- readRDS("data/kid_beats.rds") %>% 
+    mutate(age_group = sprintf("%syo", age_group) %>% recode("eyo" = "adults"),
+           tempo = recode(tempo, "fa" = "fast", "sl" = "slow"), 
+           setting = recode(setting, "ac" = "metronome", "so" = "social"), 
+           beat_ability = .5*(beat_prod + beat_perc), 
+           precision = -log_sd_ioi) 
+  scenario2 <- readRDS("data/scenario2.rds")
   assign("covox", covox, globalenv())
   assign("mer", mer, globalenv())
   assign("mer_mir_features", mer_mir_features, globalenv())
   assign("mer_emo_vars", c("angry", "sad", "happy", "peacef", "tender", "fearf"), globalenv())
+  assign("kid_beats", kid_beats, globalenv())
+  assign("scenario2", scenario2, globalenv())
   
 }
