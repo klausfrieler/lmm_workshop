eab177890b5196852624d53656ca338aa0ec4709
klausfrieler <kf@omniversum.de>
2025-03-15 20:36:47 +0100

    Some util functions

--- utils.R
+++ utils.R
@@ -33,3 +33,52 @@ get_sig <- function(p_values, levels = 3, labels = "stars"){
   if(levels > 2) stars[p_values < .001] <- sig_labels[4]
   stars
 }
+
+lm_by_hand <- function(data, nrows = 3){
+  if(is.null(nrows)){
+    nrows <- nrow(data)
+  }
+  data <- data %>% slice(1:nrows)
+  mm <- data %>% select(x, z) %>% as.matrix()
+  mm <- cbind(rep(1, nrows), mm)
+  y <- data %>% pull(y)
+  betas_man <- inv(t(mm) %*% mm) %*% t(mm) %*% y
+  betas_lm <- lm(y ~ x + z, data = data) %>% coef()
+  mean((betas_man - betas_lm)^2)
+}
+
+
+euclid_dist <- function(x, y){
+  sqrt(scalar_prod(x - y))
+}
+norm_vec <- function(x){
+  sqrt(sum(x * x))
+}
+
+unit_vec <- function(x){
+  x/norm_vec(x)
+}
+
+scalar_prod <- function(x, y){
+  sum(x * y)
+}
+
+
+center <- function(x){
+  x - mean(x)
+}
+
+sd_by_hand <- function(x){
+  n <- length(x)
+  norm_vec(center(x))/sqrt(n-1)
+}
+
+scale_by_hand <- function(x){
+  n <- length(x)
+  sqrt(n-1) * unit_vec(center(x))
+}
+
+
+cor_by_hand <- function(x, y){
+  scalar_prod(unit_vec(center(x)), unit_vec(center(y)))
+}
